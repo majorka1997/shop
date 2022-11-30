1b88c576be734bd9f1ad56af98b858952a108601
majorka1997 <majorkagame@gmail.com>
2022-11-30 22:42:28 +0300

    start

---
+++ dev/assets/scripts/main.js
@@ -0,0 +1,38 @@
+// var app = 1;
+let script = 2;
+const NUMBER = 3;
+let app = null;
+
+// script = 5;
+// console.log('script5', typeof script);
+
+// script = '6';
+// console.log('script6', typeof(script));
+
+number = 5;
+app = 1;
+
+console.log('number', number);
+console.log('app', app);
+
+// app = undefined;
+
+/* 
+string  // "fdsfsdfs", 'dsadasd', `sadsadas`
+number  // 1, 2, 3,  .... 
+NaN    // 'test'/1 = NaN
+Infinity  // 1 / 0 = Infinity, -Infinity
+BigInt  // -+(2^53 - 1)
+Boolean // true, false
+Object // {}
+symbol,
+typeof // typeof x, typeof(x)
+undefined
+null
+*/
+
+// const obj = {
+//   key: value,
+// };
+
+// console.log('script', script);
