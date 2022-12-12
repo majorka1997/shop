ef30454cba7416aa109dca93e47d287815ea11d5
majorka1997 <majorkagame@gmail.com>
2022-12-12 11:47:30 +0300

    filtr

--- dev/assets/scripts/main.js
+++ dev/assets/scripts/main.js
@@ -12,8 +12,8 @@ let app = null;
 number = 5;
 app = 1;
 
-console.log('number', number);
-console.log('app', app);
+console.log("number", number);
+console.log("app", app);
 
 // app = undefined;
 
@@ -36,3 +36,24 @@ null
 // };
 
 // console.log('script', script);
+
+// ФИЛЬТР
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtЗначение = a[i].textСодержание || a[i].innerText;
+    if (txtЗначение.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
